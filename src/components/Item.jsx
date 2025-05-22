a79da1180e758fa2614773d6aa60141a7f44ebde
leela krishna <pji.leela.krishna@kalvium.community>
2025-05-22 11:48:49 +0530

    implemented

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,8 +1,15 @@
-const Item = ({ item }) => {
-
-    // Render a single item
-    // Add a Delete and Edit button
-    return null;
-};
-
-export default Item;
+const Item = ({ item, onDelete }) => {
+    return (
+      <div style={{ marginBottom: "10px" }}>
+        <span>{item.name}</span>
+        <button onClick={() => onDelete(item.id)} style={{ marginLeft: "10px" }}>
+          Delete
+        </button>
+        {/* Optional Edit Button */}
+        {/* <button style={{ marginLeft: "5px" }}>Edit</button> */}
+      </div>
+    );
+  };
+  
+  export default Item;
+  
